c891adc6b77719382a06be8a1af86ec1d6cfa7e8
James Mueller <jamesloganmueller@gmail.com>
2023-04-21 14:09:32 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "mytrueweight-40094",
-  url: "https://mytrueweight-40094.botics.co",
-  api: "https://mytrueweight-40094.botics.co/api/v1"
+  "name": "mytrueweight-40094",
+  "url": "https://mytrueweight-40094.botics.co",
+  "api": "https://mytrueweight-40094.botics.co/api/v1",
+  "initialRoute": "Untitled1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
