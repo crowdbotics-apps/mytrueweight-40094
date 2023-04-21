9f31465a244e134392595c6491ac966ecfc62a45
James Mueller <jamesloganmueller@gmail.com>
2023-04-21 14:09:23 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "mytrueweight-40094",
   url: "https://mytrueweight-40094.botics.co",
   api: "https://mytrueweight-40094.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
