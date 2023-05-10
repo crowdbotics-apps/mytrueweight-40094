de64740d57a4a36e42e48ad3630c6bbc2da22a24
James Mueller <jamesloganmueller@gmail.com>
2023-05-10 09:26:38 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/Untitled4/index.js
+++ screens/Untitled4/index.js
@@ -1,25 +1,60 @@
-import { Text } from "react-native";
+import { Pressable } from "react-native";
+import { View } from "react-native";
+import { TextInput } from "react-native";
 import { StyleSheet, SafeAreaView } from "react-native";
 
 const Untitled4 = () => {
   return <SafeAreaView style={styles.safeArea}>
-      <Text style={styles.KCIhOOGH}>James Logan Mueller</Text>
-    </SafeAreaView>;
+      
+    <TextInput style={styles.mbCEVYYz}></TextInput><TextInput style={styles.vZtwsLcB}></TextInput><TextInput style={styles.DWmPpOOH}></TextInput><Pressable><View style={styles.UHqLMmfR}></View></Pressable></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     height: "100%"
   },
-  KCIhOOGH: {
-    width: 355,
-    height: 75,
-    lineHeight: 77,
-    fontSize: 25,
-    borderRadius: 0,
-    textAlign: "center",
+  mbCEVYYz: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30,
     position: "absolute",
-    left: 1
+    left: 103.5,
+    top: 100
+  },
+  vZtwsLcB: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30,
+    position: "absolute",
+    left: 103.5,
+    top: 150,
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "stretch",
+    justifyContent: "center"
+  },
+  DWmPpOOH: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 150,
+    height: 30,
+    position: "absolute",
+    flexDirection: "row",
+    flex: 1,
+    top: 50,
+    left: 103.5
+  },
+  UHqLMmfR: {
+    height: 35,
+    width: 142,
+    backgroundColor: "#7eace8",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    left: 107.5,
+    top: 253
   }
 });
 export default Untitled4;
